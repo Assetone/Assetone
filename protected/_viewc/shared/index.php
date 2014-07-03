e32cda605775c90a7fa1e0614584141514012afc
philip.zeh@solutions2share.net <philip.zeh@solutions2share.net>
2014-07-03 09:19:36 +0200

    REST Api  getUsers

---
+++ protected/_viewc/shared/index.php
@@ -0,0 +1,12 @@
+<?php include Doo::conf()->SITE_PATH .  Doo::conf()->PROTECTED_FOLDER . "viewc//shared/header.php"; ?>
+
+<body>
+<?php include Doo::conf()->SITE_PATH .  Doo::conf()->PROTECTED_FOLDER . "viewc//shared/body-header.php"; ?>
+	
+
+<?php include Doo::conf()->SITE_PATH .  Doo::conf()->PROTECTED_FOLDER . "viewc//shared/body-sidebar.php"; ?>
+
+
+	
+
+
