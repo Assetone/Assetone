8892abbbd8ca9567e8d774708d9024255b4c4b94
unknown <BetzF@AVANUL0133.kt.group.local>
2014-08-30 13:12:11 +0200

    Upload of latest "cubie-version"

---
+++ old/protected/viewc/index.php
@@ -0,0 +1,16 @@
+<?php include Doo::conf()->SITE_PATH .  Doo::conf()->PROTECTED_FOLDER . "viewc//shared/index.php"; ?>
+<?php include "{$data['template']}.php"; ?>
+
+		</div>
+
+		
+		
+	</div>
+	
+</div>
+
+
+<div class="page-footer"></div>
+		
+</body>
+</html>
