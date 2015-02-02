d344366c7e4b1cbf82430c41517496fa1d750247
Y56380X <ueberstimmen@gmail.com>
2015-02-02 17:59:16 +0100

    Frontpage

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<script type="text/javascript" src="js/jquery-1.11.2.js"></script>
+		<script type="text/javascript" src="js/jquery-ui.js"></script>
+	</head>
+	<body>
+		<?php
+			if(isset($_GET["action"]))
+			{
+				switch($_GET["action"])
+				{
+					default:
+				}
+			}
+		?>
+	</body>
+</html>
