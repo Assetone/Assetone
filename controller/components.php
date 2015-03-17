d3dfb16e9eaecdef970edbdefdf73210ebf1fbe0
Y56380X <ueberstimmen@gmail.com>
2015-03-17 21:05:00 +0100

    Added functions for showing components in components-controller

--- controller/components.php
+++ controller/components.php
@@ -3,14 +3,23 @@
 // Datum: 01.03.2015
 
 include ("../include/SQLManager.php");
+include ("../include/SettingsManager.php");
 
 if(isset($_GET["show"]))
 {
+	$sqlManager = initializeSQLManager($settingsManager);
 	
+	echo json_encode(mysqli_fetch_assoc($sqlManager->query("SELECT * FROM components;")));
 }
 
 if (isset($_GET["new"]))
 {
 	
+}
+
+function initializeSQLManager($settingsManager)
+{
+	$dbConfig = $settingsManager->Get("dbConfig");
+	return new SQLManager($dbConfig["username"], $dbConfig["userpassword"], "assetone");
 }
 ?>
