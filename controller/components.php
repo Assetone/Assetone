8e8a614d89e1969d720f5e22bdbf545673c9c6ed
Y56380X <ueberstimmen@gmail.com>
2015-05-14 11:17:59 +0200

    Create components
    Now it's possible to create components.

--- controller/components.php
+++ controller/components.php
@@ -20,7 +20,28 @@ if(isset($_GET["show"]))
 
 if (isset($_GET["new"]))
 {
+	// Are all arguments given
+	if (!isset($_POST["componentname"]) || !isset($_POST["componenttype"]))
+		return;
 	
+	$sqlManager = initializeSQLManager($settingsManager);
+	
+	$queryResult = $sqlManager->query("INSERT INTO components (name, ctID) VALUES ('".
+		$sqlManager->prepareValue($_POST["componentname"])	."', ".
+		$sqlManager->prepareValue($_POST["componenttype"]) .");");
+}
+
+if (isset($_GET["componenttypes"]))
+{
+	$sqlManager = initializeSQLManager($settingsManager);
+	
+	$queryResult = $sqlManager->query("SELECT ID, name FROM componenttypes;");
+	$resultTable = [];
+	
+	while ($row = mysqli_fetch_assoc($queryResult))
+		$resultTable[] = $row;
+	
+	echo json_encode($resultTable);
 }
 
 function initializeSQLManager($settingsManager)
