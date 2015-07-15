9999305fb64a32b74b12c92f55f31df19f9643e4
Y56380X <ueberstimmen@gmail.com>
2015-07-15 18:49:56 +0200

    Added database tables

--- setupfiles/database.sql
+++ setupfiles/database.sql
@@ -1,4 +1,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `assetone` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `assetone`;
@@ -27,6 +29,18 @@ CREATE TABLE IF NOT EXISTS `rooms` (
   `description` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE IF NOT EXISTS `user` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `ugID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `usergroup` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 ALTER TABLE `componentrelations`
   ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `cIDbasis_builtin` (`cIDbasis`,`cIDbuiltin`), ADD KEY `cIDbuiltin` (`cIDbuiltin`);
@@ -40,6 +54,12 @@ ALTER TABLE `componenttypes`
 ALTER TABLE `rooms`
   ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `name` (`name`);
 
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`ID`), ADD KEY `ugID` (`ugID`);
+
+ALTER TABLE `usergroup`
+  ADD PRIMARY KEY (`ID`);
+
 
 ALTER TABLE `componentrelations`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
@@ -49,11 +69,19 @@ ALTER TABLE `componenttypes`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `rooms`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `usergroup`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `componentrelations`
 ADD CONSTRAINT `componentrelations_ibfk_1` FOREIGN KEY (`cIDbasis`) REFERENCES `components` (`ID`),
 ADD CONSTRAINT `componentrelations_ibfk_2` FOREIGN KEY (`cIDbuiltin`) REFERENCES `components` (`ID`);
 
 ALTER TABLE `components`
-ADD CONSTRAINT `components_ibfk_2` FOREIGN KEY (`rID`) REFERENCES `rooms` (`ID`),
-ADD CONSTRAINT `components_ibfk_1` FOREIGN KEY (`ctID`) REFERENCES `componenttypes` (`ID`);
+ADD CONSTRAINT `components_ibfk_1` FOREIGN KEY (`ctID`) REFERENCES `componenttypes` (`ID`),
+ADD CONSTRAINT `components_ibfk_2` FOREIGN KEY (`rID`) REFERENCES `rooms` (`ID`);
+
+ALTER TABLE `user`
+ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`ugID`) REFERENCES `usergroup` (`ID`);
+COMMIT;
